b5269afeb7357d345a3cf2a10602fb0cd5e89570
varchasva rana <varchasvarana09@gmail.com>
2024-02-06 19:54:06 +0530

    check for the convergence to save time and memory

--- cube_root.py
+++ cube_root.py
@@ -1,10 +1,17 @@
 """
 this code will calculate the cube root of the number using newtons method
 """
-x = 8
 def myroot(x):
 	ig = 1.0 #initial guess
 	mi = 100 #max iterations
+	tol = 1.0e-15
 	for i in range(mi):
+		ig0 = ig
 		ig = (2*ig + (x/(ig**2)))/3
+		print(f"the value of root at iteration {i} is {ig:10.34f}")
+		delta = ig-ig0
+		if abs(delta)/x<tol:
+			break
+	       
+		
 	print(ig)
